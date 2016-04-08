c924825f8b3b0abe5f26ad1e8f4a26c10b06877a
Haokun Luo <haluo@expedia.com>
2016-04-08 11:07:46 -0700

    Before 2016

--- Practice/Main.java
+++ Practice/Main.java
@@ -36,34 +36,73 @@ public class Main implements Runnable {
     //////////////////////////////////////////
     // Hard core function
     public void solve() throws Exception {
-        long maxLen = lread();
-        for (long i = 0; i <= maxLen; i++) {
-            maxDeletedLen.put(i, maxLen + 1);
+        int R = iread(), C = iread(), N = iread();
+
+        if (N == 0) {
+            out.write("0");
+            return;
         }
-        maxDeletedLen.put(0L, 0L);
-        for (long i = 0; i < maxLen; i++) {
-            long cur = maxDeletedLen.get(i) + 1;
-            maxDeletedLen.put(i + 1, Math.min(maxDeletedLen.get(i+1), cur));
-            long rev = reverse(i);
-            if (rev <= maxLen) {
-                maxDeletedLen.put(rev, Math.min(maxDeletedLen.get(rev), cur));
-            }
+        int result;
+        if (R == 1 || C == 1) {
+            result = countForOneCase(R * C, N);
+        } else if (R == 2 || C == 2) {
+            result = countForTwoCase(R * C / 2, N);
+        } else {
+            result = countForOtherCase(R, C, N);
         }
-        out.write(String.valueOf(maxDeletedLen.get(maxLen)));
+
+        out.write(String.valueOf(result));
     }
 
-    private long reverse(long number)
-    {
-        long result = 0;
-        long remainder;
-        while (number > 0)
-        {
-            remainder = number % 10;
-            number = number / 10;
-            result = result * 10 + remainder;
+    private int countForOneCase(int notOneNumber, int N) {
+        int mid = 0;
+        if (isOddNumber(notOneNumber)) {
+            mid = notOneNumber / 2 + 1;
+        } else {
+            mid = notOneNumber / 2;
         }
+        if (N <= mid) {
+            return 0;
+        } else {
+            return (N - mid) * 2;
+        }
+    }
 
-        return result;
+    private int countForTwoCase(int notTwoNumber, int N) {
+        int mid = notTwoNumber;
+        if (N <= mid) {
+            return 0;
+        } else if (N <= mid + 2) {
+            return (N - mid) * 2;
+        } else {
+            return (N - mid - 2) * 3 + 4;
+        }
+    }
+
+    private int countForOtherCase(int R, int C, int N) {
+        if (R * C % 4 != 0) {
+            if (N <= R * C / 2) {
+                return 0;
+            } else if (N <= R * C / 2 + 2) {
+                return (N - R * C / 2 ) * 2;
+            } else if (N <= (R * C) / 2 + R + C - 2) {
+                return (N - R * C / 2 - 2) * 3 + 4;
+            } else {
+                return 4 * N - 2 * R * C - R - C;
+            }
+        } else {
+            if (N <= R * C / 2 + 1) {
+                return 0;
+            } else if (N <= (R * C) / 2 + R + C - 1) {
+                return (N - R * C / 2 - 1) * 3;
+            } else {
+                return 4 * N - 2 * R * C - R - C;
+            }
+        }
+    }
+
+    private boolean isOddNumber(int i) {
+        return (i % 2) == 1;
     }
 
     //////////////////////////////////////////
@@ -81,12 +120,12 @@ public class Main implements Runnable {
     public void run() {
         try {
             // Helper in-&-out for local test
-//            in = new BufferedReader(new InputStreamReader(System.in));
-//            out = new BufferedWriter(new OutputStreamWriter(System.out));
+            in = new BufferedReader(new InputStreamReader(System.in));
+            out = new BufferedWriter(new OutputStreamWriter(System.out));
 
             // For real file input and output
-            in = new BufferedReader(new FileReader(filename + ".in"));
-            out = new BufferedWriter(new FileWriter(filename + ".out"));
+//            in = new BufferedReader(new FileReader(filename + ".in"));
+//            out = new BufferedWriter(new FileWriter(filename + ".out"));
             solve_gcj();
             out.flush();
         } catch (Exception e) {
